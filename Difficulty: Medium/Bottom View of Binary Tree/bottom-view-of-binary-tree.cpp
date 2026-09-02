2f73b5f5ea8054f900bb5e292884c47c85da9ad7
Chaudhary Naman Payal <namanpayal10@gmail.com>
2026-09-02 19:28:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
@@ -0,0 +1,61 @@
+/*
+Definition for Node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void find(Node* root,int pos,int&l,int&r){
+        if(!root){
+            return;
+        }
+        l=min(l,pos);
+        r=max(r,pos);
+        find(root->left,pos-1,l,r);
+        find(root->right,pos+1,l,r);
+    }
+    
+    vector<int> bottomView(Node *root) {
+        // code here
+        int l=0,r=0;
+        find(root,0,l,r);
+        vector<int>ans(r-l+1);
+        vector<bool>filled(r-l+1,0);
+        queue<Node*>q;
+        queue<int>index;
+        q.push(root);
+        index.push(-1*l);
+        while(!q.empty()){
+            Node* temp = q.front();
+            q.pop();
+
+            int pos = index.front();
+            index.pop();
+
+                ans[pos]=temp->data;
+
+            if(temp->left){
+                q.push(temp->left);
+                index.push(pos-1);
+            }
+
+            if(temp->right){
+                q.push(temp->right);
+                index.push(pos+1);
+            }
+        }
+
+        return ans;
+        
+    }
+};
