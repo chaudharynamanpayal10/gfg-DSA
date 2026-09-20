63fb57f28ec20308bb9377adc64d2f5268ade5bd
Chaudhary Naman Payal <namanpayal10@gmail.com>
2026-09-20 11:59:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Iterative Preorder Traversal/iterative-preorder-traversal.cpp
@@ -0,0 +1,36 @@
+/* Binary Tree Node Structure
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    vector<int> preOrder(Node* root) {
+        // code here
+        stack<Node*>s;
+        s.push(root);
+        vector<int>ans;
+        
+        while(!s.empty()){
+            Node *temp = s.top();
+            s.pop();
+            ans.push_back(temp->data);
+            
+            if(temp->right){
+                s.push(temp->right);
+            }
+             if(temp->left){
+                s.push(temp->left);
+            }
+        }
+        return ans;
+    }
+};
