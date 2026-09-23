558ee9394357310662280dbe2c184671166cb40c
Chaudhary Naman Payal <namanpayal10@gmail.com>
2026-09-23 19:36:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Iterative Preorder Traversal/iterative-preorder-traversal.cpp
+++ Difficulty: Medium/Iterative Preorder Traversal/iterative-preorder-traversal.cpp
@@ -15,22 +15,43 @@ class Solution {
   public:
     vector<int> preOrder(Node* root) {
         // code here
-        stack<Node*>s;
-        s.push(root);
         vector<int>ans;
+                while(root){
+
+                    //Left part doesnot exist...
+
+                    if(!root->left){
+                        ans.push_back(root->data);
+                        root=root->right;
+                    }
+
+                    //left part exist...
+
+                    else{
+                        Node* curr = root->left;
+
+                        while(curr->right && curr->right!=root){
+                            curr=curr->right;
+                        }
+
+                            //left Subtree not traverse...
+
+                            if(curr->right==NULL){
+                                ans.push_back(root->data);
+                                curr->right=root;
+                                root=root->left;
+                            }
+
+                            //already traverse...
+
+                            else{
+                                curr->right=NULL;
+                                root=root->right;
+                            }
+                    }
+                }
+
+                return ans;
         
-        while(!s.empty()){
-            Node *temp = s.top();
-            s.pop();
-            ans.push_back(temp->data);
-            
-            if(temp->right){
-                s.push(temp->right);
-            }
-             if(temp->left){
-                s.push(temp->left);
-            }
-        }
-        return ans;
     }
 };
